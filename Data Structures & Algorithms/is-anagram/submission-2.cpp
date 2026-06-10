953f42f0e6aa73f48dc6ed5efc3c743723799153
Dev_girl <138586779+yochna@users.noreply.github.com>
2026-06-10 18:57:00 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        unordered_map<char,int>mp;
+        for(char ch:s){
+mp[ch]++;
+        }
+        for(char ch:t){
+            if(mp[ch]==0)return false;
+            mp[ch]--;
+        }
+        return true;
+    }
+};
